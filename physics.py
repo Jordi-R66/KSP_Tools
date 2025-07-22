829aa7431ce4407b04d1eabd6e32fdbbc751478c
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-22 20:31:16 +0200

    New functions in physics.py

--- physics.py
+++ physics.py
@@ -17,6 +17,24 @@ def stdGravParam(mass: float) -> float:
 	"""Returns the gravitational parameter of a specific mass"""
 	return mass * G
 
+def meanMotion(mass: float, Mass: float, sma: float) -> float:
+	mu: float = stdGravParam(mass) + stdGravParam(Mass)
+
+	return sqrt(mu / sma**3)
+
+def meanAnomalyAtUT(mass: float, Mass: float, sma: float, mean_anomaly: float, epoch: float, UT: float) -> float:
+		mean_motion: float = meanMotion(mass, Mass, sma)
+
+		mean_ano_UT: float = mean_anomaly + (UT - epoch) * mean_motion
+		mean_ano_UT %= 2 * pi
+
+		return mean_ano_UT
+
+def orbitalPeriod(mass: float, Mass: float, sma: float) -> float:
+	mu: float = stdGravParam(mass) + stdGravParam(Mass)
+
+	return 2 * pi * sqrt(sma ** 3 / mu)
+
 def sphereOfInfluence(mass: float, Mass: float, distance: float) -> float:
 	"""Returns the SOI's radius of a given mass"""
 
@@ -41,3 +59,20 @@ def apoapsis(sma: float, ecc: float) -> float:
 def periapsis(sma: float, ecc: float) -> float:
 	return sma * (1.0 - ecc)
 
+def orbitalSpeed_Elliptical(mass: float, Mass: float, altitude: float, sma: float) -> float:
+	mu: float = stdGravParam(mass) + stdGravParam(Mass)
+
+	return sqrt(mu * (2.0 / altitude - 1.0 / sma))
+
+def orbitalSpeed_Circular(mass: float, Mass: float, sma: float) -> float:
+	return orbitalSpeed_Elliptical(mass, Mass, sma, sma)
+
+def orbitalSpeed_Parabolic(mass: float, Mass: float, altitude: float) -> float:
+	mu: float = stdGravParam(mass) + stdGravParam(Mass)
+
+	return sqrt(mu * (2.0 / altitude))
+
+def orbitalSpeed_Hyperbolic(mass: float, Mass: float, altitude: float, sma: float) -> float:
+	mu: float = stdGravParam(mass) + stdGravParam(Mass)
+
+	return sqrt(mu * (2.0 / altitude + 1.0 / sma))
